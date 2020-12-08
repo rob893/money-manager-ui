b095a7072850eb484620c00c6ae17f57ccc70515
Robert Herber <rwherber@gmail.com>
2020-12-08 07:56:16 -0800

    chore: move header to component

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,28 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-cog</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app clipped-left>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-    </v-app-bar>
+  <v-app>
+    <Header />
 
     <v-main>
-      <!-- Provides the application the proper gutter -->
       <v-container fluid>
-        <!-- If using vue-router -->
-        <router-view></router-view>
+        <router-view />
       </v-container>
     </v-main>
 
-    <Footer></Footer>
+    <Footer />
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
+    Header,
     Footer
-  },
-
-  data: () => ({
-    drawer: false,
-    title: process.env.VUE_APP_TITLE
-  })
+  }
 });
 </script>
