67765f755719e26e5da6b0bfafb9d1ad10f031a3
Robert Herber <rwherber@gmail.com>
2020-12-27 00:24:18 -0800

    chore: work on login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,37 +4,40 @@
       <v-row align="center" justify="center">
         <v-col>
           <v-card class="mx-auto" elevation="2" max-width="400">
-            <v-card-title class="justify-center">Login</v-card-title>
+            <v-form ref="form" v-model="formValid" @submit.prevent="login">
+              <v-card-title class="justify-center">Login</v-card-title>
 
-            <v-card-text>
-              <v-alert v-if="errorMessage" type="error">{{ errorMessage }}</v-alert>
+              <v-card-text>
+                <v-alert v-model="showErrorAlert" type="error" dismissible>{{ errorMessage }}</v-alert>
 
-              <v-form ref="form" v-model="formValid">
                 <v-text-field v-model="username" :rules="usernameRules" label="Username" required />
                 <v-text-field v-model="password" type="password" :rules="passwordRules" label="Password" required />
-              </v-form>
-            </v-card-text>
+              </v-card-text>
 
-            <v-card-actions v-if="loading" class="justify-center">
-              <v-progress-circular indeterminate color="primary"></v-progress-circular>
-            </v-card-actions>
+              <div v-if="!loading">
+                <v-card-actions class="justify-center">
+                  <v-btn :disabled="!formValid" color="success" class="mr-4" type="submit" @click="login">Login</v-btn>
+                  <v-btn color="error" class="mr-4" @click="resetForm">Clear</v-btn>
+                </v-card-actions>
 
-            <v-card-actions v-if="!loading" class="justify-center">
-              <v-btn :disabled="!formValid" color="success" class="mr-4" type="submit" @click="login">Login</v-btn>
-              <v-btn color="error" class="mr-4" @click="resetForm">Clear</v-btn>
-            </v-card-actions>
+                <v-card-text v-if="!loading" class="text-center"><strong>Or</strong></v-card-text>
 
-            <v-card-text v-if="!loading" class="text-center"><strong>Or</strong></v-card-text>
+                <v-card-actions v-if="!loading" class="justify-center">
+                  <v-btn color="white" class="google-btn" @click="googleLogin"
+                    ><img src="img/google-logo.png" alt="Google logo" class="google-btn-logo" /><span
+                      class="google-btn-text"
+                      >Continue with Google</span
+                    ></v-btn
+                  >
+                </v-card-actions>
+              </div>
 
-            <v-card-actions v-if="!loading" class="justify-center">
-              <v-btn color="white" class="google-btn" @click="googleLogin"
-                ><img src="img/google-logo.png" alt="Google logo" class="google-btn-logo" /><span
-                  class="google-btn-text"
-                  >Continue with Google</span
-                ></v-btn
-              >
-            </v-card-actions>
-            <br />
+              <v-card-actions v-else class="justify-center">
+                <v-progress-circular indeterminate color="primary"></v-progress-circular>
+              </v-card-actions>
+
+              <br />
+            </v-form>
           </v-card>
         </v-col>
       </v-row>
@@ -59,12 +62,14 @@ export default Vue.extend({
     passwordRules: [(password: string) => !!password || 'Password is required'],
     formValid: false,
     errorMessage: null as string | null,
+    showErrorAlert: false,
     loading: false
   }),
 
   methods: {
     async googleLogin(): Promise<void> {
       this.loading = true;
+      this.clearErrorMessage();
 
       try {
         const googleUser = await googleAuthService.signIn();
@@ -73,12 +78,10 @@ export default Vue.extend({
         this.errorMessage = null;
         this.$router.push({ name: RouteName.Budgets });
       } catch (error) {
-        if (TypeGuards.isAxiosError(error) && error.response?.status === 401) {
-          this.errorMessage = 'Invalid username or password.';
-        } else if (TypeGuards.isAxiosError(error) && error.response?.status === 404) {
+        if (TypeGuards.isAxiosError(error) && error.response?.status === 404) {
           this.$router.push({ name: RouteName.Register, query: { socialLogin: LinkedAccountType.Google } });
         } else {
-          this.errorMessage = 'Something went wrong.';
+          this.showErrorMessage('Unable to login with Google.');
         }
       } finally {
         this.loading = false;
@@ -86,24 +89,37 @@ export default Vue.extend({
     },
 
     async login(): Promise<void> {
-      if (this.username && this.password) {
-        try {
-          this.loading = true;
-          await authService.login(this.username, this.password);
-          this.errorMessage = null;
-          this.$router.push({ name: RouteName.Dashboard });
-        } catch (error) {
-          if (TypeGuards.isAxiosError(error) && error.response?.status === 401) {
-            this.errorMessage = 'Invalid username or password.';
-          } else {
-            this.errorMessage = 'Something went wrong.';
-          }
-        } finally {
-          this.loading = false;
-        }
-      } else {
-        this.errorMessage = 'Username and password are required.';
+      this.clearErrorMessage();
+
+      if (!this.username || !this.password) {
+        this.showErrorMessage('Username and password are required.');
+        return;
       }
+
+      try {
+        this.loading = true;
+        await authService.login(this.username, this.password);
+        this.errorMessage = null;
+        this.$router.push({ name: RouteName.Budgets });
+      } catch (error) {
+        if (TypeGuards.isAxiosError(error) && error.response?.status === 401) {
+          this.showErrorMessage('Invalid username or password.');
+        } else {
+          this.showErrorMessage('Unable to login.');
+        }
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    showErrorMessage(message: string): void {
+      this.errorMessage = message;
+      this.showErrorAlert = true;
+    },
+
+    clearErrorMessage(): void {
+      this.errorMessage = null;
+      this.showErrorAlert = false;
     },
 
     resetForm(): void {
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,88 +3,98 @@
     <v-container fluid fill-height>
       <v-row align="center" justify="center">
         <v-col>
-          <v-card v-if="socialLoginType === 'Google'" class="mx-auto" elevation="2" max-width="600">
-            <v-card-title>Register using Google Account</v-card-title>
+          <v-card v-if="socialLoginType === 'Google'" class="mx-auto" elevation="2" max-width="400">
+            <v-form
+              ref="registerUsingGoogleAccountForm"
+              v-model="registerUsingGoogleAccountForm.formValid"
+              @submit.prevent="resetRegisterUsingGoogleAccountForm"
+            >
+              <v-card-title class="justify-center">Register using Google Account</v-card-title>
 
-            <v-card-text>
-              <v-alert v-if="errorMessage" type="error">{{ errorMessage }}</v-alert>
+              <v-card-text>
+                <v-alert v-if="errorMessage" type="error">{{ errorMessage }}</v-alert>
 
-              <v-form ref="registerUsingGoogleAccountForm" v-model="registerUsingGoogleAccountForm.formValid">
                 <v-text-field
                   v-model="registerUsingGoogleAccountForm.username"
                   :rules="registerUsingGoogleAccountForm.usernameRules"
-                  label="Username"
+                  label="Username*"
                   required
                 />
-              </v-form>
-            </v-card-text>
 
-            <v-card-actions v-if="loading">
-              <v-progress-circular indeterminate color="primary"></v-progress-circular>
-            </v-card-actions>
+                <small>*indicates required field</small>
+              </v-card-text>
 
-            <v-card-actions v-if="!loading">
-              <v-btn
-                :disabled="!registerUsingGoogleAccountForm.formValid"
-                color="success"
-                class="mr-4"
-                @click="registerUsingGoogleAccount"
-                >Continue</v-btn
-              >
-              <v-btn color="error" class="mr-4" @click="resetRegisterUsingGoogleAccountForm">Clear</v-btn>
-            </v-card-actions>
+              <v-card-actions v-if="!loading" class="justify-center">
+                <v-btn :disabled="!registerUsingGoogleAccountForm.formValid" color="success" class="mr-4" type="submit"
+                  >Continue</v-btn
+                >
+                <v-btn color="error" class="mr-4" @click="resetRegisterUsingGoogleAccountForm">Clear</v-btn>
+              </v-card-actions>
+
+              <v-card-actions v-else class="justify-center">
+                <v-progress-circular indeterminate color="primary"></v-progress-circular>
+              </v-card-actions>
+            </v-form>
           </v-card>
 
-          <v-card v-else class="mx-auto" elevation="2" max-width="600">
-            <v-card-title>Register</v-card-title>
+          <v-card v-else class="mx-auto" elevation="2" max-width="400">
+            <v-form ref="registerForm" v-model="registerForm.formValid" @submit.prevent="register">
+              <v-card-title class="justify-center">Register</v-card-title>
 
-            <v-card-text>
-              <v-alert v-if="errorMessage" type="error">{{ errorMessage }}</v-alert>
+              <v-card-text>
+                <v-alert v-if="errorMessage" type="error">{{ errorMessage }}</v-alert>
 
-              <v-form ref="registerForm" v-model="registerForm.formValid">
                 <v-text-field
                   v-model="registerForm.username"
                   :rules="registerForm.usernameRules"
-                  label="Username"
+                  label="Username*"
                   required
                 />
+
                 <v-text-field
                   v-model="registerForm.password"
                   type="password"
                   :rules="registerForm.passwordRules"
-                  label="Password"
+                  label="Password*"
                   required
                 />
+
+                <v-text-field type="password" :rules="confirmPasswordRules" label="Confirm Password*" required />
+
                 <v-text-field
                   v-model="registerForm.firstName"
                   :rules="registerForm.firstNameRules"
-                  label="First Name"
+                  label="First Name*"
                   required
                 />
+
                 <v-text-field
                   v-model="registerForm.lastName"
                   :rules="registerForm.lastNameRules"
-                  label="Last Name"
+                  label="Last Name*"
                   required
                 />
+
                 <v-text-field
                   v-model="registerForm.email"
                   type="email"
                   :rules="registerForm.emailRules"
-                  label="Email"
+                  label="Email*"
                   required
                 />
-              </v-form>
-            </v-card-text>
 
-            <v-card-actions v-if="loading">
-              <v-progress-circular indeterminate color="primary"></v-progress-circular>
-            </v-card-actions>
+                <small>*indicates required field</small>
+              </v-card-text>
 
-            <v-card-actions v-if="!loading">
-              <v-btn :disabled="!registerForm.formValid" color="success" class="mr-4" @click="register">Continue</v-btn>
-              <v-btn color="error" class="mr-4" @click="resetRegisterForm">Clear</v-btn>
-            </v-card-actions>
+              <v-card-actions v-if="!loading" class="justify-center">
+                <v-btn :disabled="!registerForm.formValid" color="success" class="mr-4" type="submit">Continue</v-btn>
+                <v-btn color="error" class="mr-4" @click="resetRegisterForm">Clear</v-btn>
+              </v-card-actions>
+
+              <v-card-actions v-else class="justify-center">
+                <v-progress-circular indeterminate color="primary"></v-progress-circular>
+              </v-card-actions>
+            </v-form>
           </v-card>
         </v-col>
       </v-row>
@@ -116,7 +126,8 @@ export default Vue.extend({
       passwordRules: [
         (password: string) => !!password || 'Password is required',
         (password: string) =>
-          (password.length >= 6 && password.length <= 256) || 'Password must be between 6 and 256 characters long.'
+          (password && password.length >= 6 && password.length <= 256) ||
+          'Password must be between 6 and 256 characters long.'
       ],
       firstName: null as string | null,
       firstNameRules: [(firstName: string) => !!firstName || 'First name is required'],
@@ -149,7 +160,7 @@ export default Vue.extend({
           email
         });
         this.errorMessage = null;
-        this.$router.push({ name: RouteName.Dashboard });
+        this.$router.push({ name: RouteName.Budgets });
       } catch (error) {
         this.errorMessage = 'Something went wrong.';
       } finally {
@@ -169,7 +180,7 @@ export default Vue.extend({
         this.loading = true;
         await authService.registerUserUsingGoogleAccount(username, idToken);
         this.errorMessage = null;
-        this.$router.push({ name: RouteName.Dashboard });
+        this.$router.push({ name: RouteName.Budgets });
       } catch (error) {
         this.errorMessage = 'Something went wrong.';
       } finally {
@@ -185,6 +196,12 @@ export default Vue.extend({
     resetRegisterUsingGoogleAccountForm(): void {
       this.errorMessage = null;
       (this.$refs.registerUsingGoogleAccountForm as any).reset();
+    }
+  },
+
+  computed: {
+    confirmPasswordRules(): ((confirmPw: string) => boolean | string)[] {
+      return [(confirmPw: string) => confirmPw === this.registerForm.password || 'Passwords must match.'];
     }
   },
 
